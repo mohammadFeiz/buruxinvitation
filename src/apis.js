c3067c9fb18e1f0474bb9a5b0784ab6063de9d10
MrMHappy <105713803+MehdiShad@users.noreply.github.com>
2023-06-07 18:31:15 +0330

    .

--- src/apis.js
+++ src/apis.js
@@ -1,6 +1,6 @@
 import AIODate from "./npm/aio-date/aio-date";
-const hostName = `http://172.16.7.34:8002`;
-// const hostName = `http://192.168.10.50:8086`;
+// const hostName = `http://172.16.7.34:8002`;
+const hostName = `http://192.168.10.50:8086`;
 //const hostName = `http://172.16.7.34:8002`
 // const hostName = `https://u.davat.app`
 // const hostName = `http://localhost:8002`
